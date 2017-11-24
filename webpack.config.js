11b7229c83033deced3948505aa927aea689bc20
Euirim Choi <euirim@me.com>
2017-11-24 04:54:11 -0600

    Integrate webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
-module.exports = {
+var config = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+ }
 };
+
+module.exports = config;
