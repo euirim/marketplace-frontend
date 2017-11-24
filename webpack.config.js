5f2b90bce34b89e318fd8aabee77f7f6dc806afd
Euirim Choi <euirim@me.com>
2017-11-24 04:56:07 -0600

    Implement babel loaders for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,19 @@ var config = {
   devServer: {
     inline: true,
     port: 3000
- }
+ },
+ module: {
+  loaders: [
+     {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+           presets: ['es2015', 'react']
+        }
+     }
+    ]
+  }
 };
 
 module.exports = config;
