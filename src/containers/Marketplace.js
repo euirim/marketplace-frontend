237fc4432bee87f02a8ed4400afa33ab7deaba2d
Euirim Choi <euirim@me.com>
2017-11-24 05:18:34 -0600

    Setup React environment with first test component

---
+++ src/containers/Marketplace.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Marketplace extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+export default Marketplace;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Marketplace from "./containers/Marketplace.js";
+
+ReactDOM.render(<Marketplace />, document.getElementById('app'));
