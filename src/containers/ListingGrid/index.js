5e1081cfe77066d266e920092ef68c5582a444d4
euirim <euirim@me.com>
2017-12-10 20:30:19 -0500

    Begin axios integration with default request library

--- src/containers/ListingGrid/index.js
+++ src/containers/ListingGrid/index.js
@@ -1,3 +1,19 @@
 import React from 'react';
 
 import { Container } from 'semantic-ui-react';
+
+export class ListingCardGrid extends React.component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            listings: []
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+    }
+}
