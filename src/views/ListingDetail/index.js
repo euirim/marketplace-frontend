399bcd784c53f2521740944926adb8950d4ac1f7
euirim <euirim@me.com>
2017-12-11 04:45:44 -0500

    Implement successful routing and api query for ListingDetail page

--- src/views/ListingDetail/index.js
+++ src/views/ListingDetail/index.js
@@ -1,13 +1,46 @@
 import React from "react";
+
+import ListingService from "services/api/listing.js";
+
 import { Container, Header, Grid } from "semantic-ui-react";
 
 
 export default class ListingDetail extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            listing: null
+        };
+    }
+
+    componentDidMount() {
+        ListingService
+            .get(this.props.match.params.id)
+            .then(res => {
+                this.setState({ listing: res });
+            });
+    }
+
     render() {
-        return (
-            <Container style={{ marginTop: '5em' }}>
-                Hello world.
-            </Container>
-        );
+        var content;
+
+        if (this.state.listing) {
+            content = (
+                <Container style={{ marginTop: '5em' }}>
+                    <Header as="h1">{ this.state.listing.name }</Header>
+                    <p>{ this.state.listing.about }</p>
+                </Container>
+            );
+        }
+        else {
+            content = (
+                 <Container style={{ marginTop: '5em' }}>
+                    <Header as="h2">Loading...</Header>
+                </Container>
+            );
+        }
+
+        return content;
     }
 }
