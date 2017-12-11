9ddf2a36acc10b82f5961579bf7793e02d3897ab
euirim <euirim@me.com>
2017-12-10 20:38:21 -0500

    Create initial api wrappers for listings

---
+++ src/services/api/listing.js
@@ -0,0 +1,15 @@
+import request from "shared/lib/request";
+
+function get(id) {
+    return request({
+        url: `/listings/${id}`,
+        method: "GET" 
+    });
+}
+
+function get_most_recent(num) {
+    return request({
+        url: `/listings`,
+        method: "GET"
+    });
+}
