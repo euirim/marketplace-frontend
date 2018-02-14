b6abd6a19ea34adb78aa6ea6a0a240216354ed1f
euirim <euirim@me.com>
2018-02-14 04:10:07 -0600

    Implement buggy recaptcha

--- src/components/InquiryForm/index.js
+++ src/components/InquiryForm/index.js
@@ -1,22 +1,30 @@
 import React from "react";
 
 import { Form, Text, Select, TextArea } from 'react-form';
-import { Container, Header, Grid, Message } from "semantic-ui-react";
+import { Button, Container, Header, Grid, Message } from "semantic-ui-react";
+import Recaptcha from "react-recaptcha";
 
 import { Redirect } from "react-router-dom";
 
 import InquiryService from "services/api/inquiry.js";
 
+let captcha;
+
 export default class InquiryForm extends React.Component {
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRecaptcha = this.handleRecaptcha.bind(this);
         this.state = {};
     }
 
     handleSubmit(submittedVals) {
+        submittedVals["g-recaptcha-response"] = this.state.recaptchaToken;
+
         submittedVals["listing"] = this.props.listingID;
         this.setState({submittedVals});
+
+        console.log(submittedVals);
 
         InquiryService
             .put(submittedVals)
@@ -26,20 +34,33 @@ export default class InquiryForm extends React.Component {
             });
     }
 
+    handleRecaptcha(response) {
+        alert("HELLO");
+        this.setState({ recaptchaToken: response });
+        document.getElementById("inquiryForm").submit();
+    }
+
     render() {
+        // manually trigger reCAPTCHA execution
+        const executeCaptcha = function () {
+            captcha.execute();
+            console.log("wow");
+            this.setState({ recaptchaToken: captcha.getResponse() });
+        };
+
         const FormContent = props => (
-            <form className="ui form" onSubmit={props.formApi.submitForm}>
+            <form id="inquiryForm" className="ui form" onSubmit={props.formApi.submitForm}>
                 <div className="field required">
                     <TextArea field="msg" id="msg" />
                 </div>
 
-                <button 
-                    className="ui button g-recaptcha" 
-                    type="submit"
-                    data-sitekey="6Lc2PkYUAAAAAMPuGU655s-wI-0xjn60GyjDOwc3"
-                >
-                    Submit
-                </button>
+                <Button onClick={executeCaptcha}>Submit</Button>
+
+                <Recaptcha
+                    rel={ el => captcha = el }
+                    size="invisible"
+                    sitekey="6Lc2PkYUAAAAAMPuGU655s-wI-0xjn60GyjDOwc3"
+                />
             </form>
         );
 
