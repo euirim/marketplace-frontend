2823b67d67369403df1fa72a72a3ac8074b48f38
Euirim Choi <euirim@me.com>
2018-02-27 01:00:37 -0600

    Implement most of card design

--- src/components/ListingCard/index.js
+++ src/components/ListingCard/index.js
@@ -4,22 +4,30 @@ import {
     Link
 } from "react-router-dom";
 
-import { Card, Image } from 'semantic-ui-react';
+import { 
+    Card, 
+    Image,
+    Segment,
+    Label,
+    Header
+} from 'semantic-ui-react';
 
 export default class ListingCard extends React.Component {
     render(){
         return (
             <Card as={ Link } to={ "/listings/" + this.props.id } color="red" centered>
-                <Image src={ this.props.photo }></Image>
-                <Card.Content>
-                    <Card.Header>{ this.props.name }</Card.Header>
-                    <Card.Meta>{ this.props.category.name }</Card.Meta>
-                </Card.Content>
+                <Image label={{ as: "a", content: this.props.category.name, attached: "top right", color: "red", className: "uppercase" }} src={ this.props.photo }></Image>
 
                 <Card.Content extra>
-                    <Card.Content textAlign="right">
-                        <p>$ { this.props.price }</p>
+                    <Card.Content className="right floated">
+                        <div className="listing-card-price-label">
+                            <Header textAlign="right" sub>Price</Header>
+                            <strong>$ { this.props.price }</strong>
+                        </div>
                     </Card.Content>
+                    <Card.Header disabled>
+                        <span className="listing-card-header">{ this.props.name }</span>
+                    </Card.Header>
                 </Card.Content>
             </Card>
         );
