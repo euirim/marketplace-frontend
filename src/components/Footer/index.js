52a2be93056fc5bb82723281940a22c82b771631
euirim <euirim@me.com>
2018-03-31 04:40:23 -0500

    Add policies URL

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,7 +19,11 @@ export default class Footer extends React.Component {
                         <List horizontal link>
                             <List.Item as="a">About Marketplace</List.Item>
                             <List.Item as="a">Advertise</List.Item>
-                            <List.Item as="a">Policies</List.Item>
+                            <List.Item 
+                                as="a" 
+                                href="https://www.chicagomaroon.com/pages/marketplace-policies/">
+                                Policies
+                            </List.Item>
                             <List.Item as="a">Contact</List.Item>
                             <List.Item as="a" href="https://www.chicagomaroon.com">The Maroon</List.Item>
                             <List.Item>&#169; 2018 The Chicago Maroon</List.Item>
