a4359235229b8e43fe0b220fd73740267a530eff
euirim <euirim@me.com>
2018-04-01 00:58:18 -0500

    Add page links in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,14 +17,22 @@ export default class Footer extends React.Component {
                 <Grid.Row>
                     <Grid.Column width={16} textAlign="center">
                         <List horizontal link>
-                            <List.Item as="a">About Marketplace</List.Item>
-                            <List.Item as="a">Advertise</List.Item>
+                            <List.Item 
+                                as="a"
+                                href="https://www.chicagomaroon.com/pages/about-marketplace/">
+                                About Marketplace
+                            </List.Item>
+                            <List.Item as="a" disabled>Advertise</List.Item>
                             <List.Item 
                                 as="a" 
                                 href="https://www.chicagomaroon.com/pages/marketplace-policies/">
                                 Policies
                             </List.Item>
-                            <List.Item as="a">Contact</List.Item>
+                            <List.Item 
+                                as="a"
+                                href="https://www.chicagomaroon.com/pages/marketplace-contact/">
+                                Contact
+                            </List.Item>
                             <List.Item as="a" href="https://www.chicagomaroon.com">The Maroon</List.Item>
                             <List.Item>&#169; 2018 The Chicago Maroon</List.Item>
                         </List>
